7cc1de351df522f25a86dd92abbfd5e09d666d2e
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-08 21:21:45 -0500

    Update Control.sol

--- Control.sol
+++ Control.sol
@@ -3,10 +3,17 @@
 pragma solidity ^ 0.8.13;
 
 contract Control {
+  address owner;
+
+  constructor() public {
+    owner = msg.sender;
+  }
+
   function sendMoney() public payable {
 
   }
   function withdrawAllMoney(address payable _to) public {
+    require(msg.sender == owner, "You are not the owner, bro.");
     _to.transfer(address(this).balance);
   }
 }
