c21088aea47496097fcdea050d72d88da4bc7ab6
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-08 21:30:23 -0500

    Update Control.sol

--- Control.sol
+++ Control.sol
@@ -4,6 +4,7 @@ pragma solidity ^ 0.8.13;
 
 contract Control {
   address owner;
+  bool public paused;
 
   constructor() public {
     owner = msg.sender;
@@ -12,8 +13,13 @@ contract Control {
   function sendMoney() public payable {
 
   }
+  function setPaused(bool _paused) public {
+    require(msg.sender == owner, "You are not the owner, bro.");
+    paused = _paused;
+  }
   function withdrawAllMoney(address payable _to) public {
     require(msg.sender == owner, "You are not the owner, bro.");
+    require(!paused, "Contract is paused.");
     _to.transfer(address(this).balance);
   }
 }
